c72a82d21149a56f1b0f5c470df2c334cc2b616d
Predrag Njegovanovic <djaps94@gmail.com>
2018-05-29 12:12:22 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+NAME = "reuters-news-graph-mining"
+AUTHOR = "Predrag Njegovanovic"
+VERSION = "1.0"
+EMAIL = "djaps94@gmail.com"
+DESCRIPTION = "Analysis of Retuers News graph properties"
+MIT = "MIT"
+URL = "https://github.com/Djaps94/reuters-news-graph-mining"
+
+setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        license=MIT,
+        url=URL,
+        packages=find_packages() + ['data'],
+        include_package_data=True
+    )
