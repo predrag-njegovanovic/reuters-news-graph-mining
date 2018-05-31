86c377d5b62aae86ef3efc9302577aea241c68cc
Predrag Njegovanovic <djaps94@gmail.com>
2018-05-31 15:02:00 +0200

    Find top influential words with centrality measures

---
+++ src/graph_analysis.py
@@ -0,0 +1,39 @@
+import operator
+import networkx as nx
+
+from src.utils import load
+from src.utils import PAJEK_FORMAT
+
+
+def degree_centrality(graph):
+    return nx.degree_centrality(graph)
+
+
+def closeness_centrality(graph):
+    return nx.closeness_centrality(graph)
+
+
+def betweenness_centrality(graph):
+    return nx.betweenness_centrality(graph, weight="weight")
+
+
+def most_influential_words(centrality_measure):
+    word_dict = sorted(centrality_measure.items(),
+                       key=operator.itemgetter(1),
+                       reverse=True)
+    influential_words = word_dict[:10]
+    noninfluent_words = word_dict[-10:]
+
+    print_function = lambda wm_pair: print("Word: {} ----> Influence: {:.4f}".format(wm_pair[0], wm_pair[1]))
+    print("Top 10 most influential words:\n")
+    for wm_pair in influential_words:
+        print_function(wm_pair)
+    print("<----------------------------------->\n")
+    print("Top 10 lowest influential words:\n")
+    for wm_pair in noninfluent_words:
+        print_function(wm_pair)
+
+
+if __name__ == "__main__":
+    G = load(PAJEK_FORMAT)
+    most_influential_words(betweenness_centrality(G))
