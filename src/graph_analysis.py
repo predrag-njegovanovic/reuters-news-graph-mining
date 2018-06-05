42395e1aeffcf33636de97a29759b21e9ce5bc09
Predrag Njegovanovic <djaps94@gmail.com>
2018-06-05 16:32:52 +0200

    Show occurrences of influental words by days

--- src/graph_analysis.py
+++ src/graph_analysis.py
@@ -2,22 +2,27 @@ import operator
 import numpy as np
 import networkx as nx
 import pandas as pd
+import sys
 
 from src.utils import load
+from src.utils import get_word_attributes
 from src.utils import PAJEK_FORMAT
 from src.utils import RESULTS_PATH
+from src.utils import DEGREE_FILE
+from src.utils import CLOSENESS_FILE
+from src.utils import BETWEENNESS_FILE
 
 
 def degree_centrality(graph):
-    return ("degree_centrality.tsv", nx.degree_centrality(graph))
+    return nx.degree_centrality(graph)
 
 
 def closeness_centrality(graph):
-    return ("closeness_centrality.tsv", nx.closeness_centrality(graph))
+    return nx.closeness_centrality(graph)
 
 
 def betweenness_centrality(graph):
-    return ("betweenness_centrality.tsv", nx.betweenness_centrality(graph, weight="weight"))
+    return nx.betweenness_centrality(graph, weight="weight")
 
 
 def most_influential_words(centrality_name, centrality_measure):
@@ -48,3 +53,32 @@ def most_influential_words(centrality_name, centrality_measure):
     print("Top 10 lowest influential words:\n")
     for wm_pair in noninfluent_words:
         print_function(wm_pair)
+
+
+def word_day_occurrences(centrality_file_path=None,
+                         centrality_measure='degree',
+                         graph_path=PAJEK_FORMAT):
+
+    if(centrality_file_path is None):
+        print("Trying to load graph from file path: {}".format(graph_path))
+        try:
+            graph = load(graph_path)
+        except FileNotFoundError:
+            print('File not found on specified path.')
+            sys.exit(1)
+
+        if centrality_measure == 'degree':
+            words = degree_centrality(graph).keys()
+        elif centrality_measure == 'closeness':
+            words = closeness_centrality(graph).keys()
+        elif centrality_measure == 'betweenness':
+            words = betweenness_centrality(graph).keys()
+        else:
+            print('Specified centrality measure not implemented.')
+            sys.exit(1)
+    else:
+        centrality_data = pd.read_csv(centrality_file_path, sep='\t')
+        words = list(centrality_data.iloc[:, 0])
+        word_day_dict = get_word_attributes(graph_path)
+        for word in words:
+            print("Word: {},  Days of occurrence: {}\n".format(word, word_day_dict[word]))
