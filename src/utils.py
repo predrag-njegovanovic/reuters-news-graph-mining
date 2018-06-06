8f776c9317d24c80e0a94395474e99f1d884dcee
Predrag Njegovanovic <djaps94@gmail.com>
2018-06-06 12:19:45 +0200

    Calculate, save and read features from files

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import networkx as nx
 import pandas as pd
 
 from os import path
+from src.graph_analysis import closeness_centrality
 from graph_tool import load_graph
 from networkx.readwrite import pajek
 
@@ -17,6 +18,10 @@ RESULTS_PATH = path.join(full_path + "/results/")
 DEGREE_FILE = "degree_centrality.tsv"
 CLOSENESS_FILE = "closeness_centrality.tsv"
 BETWEENNESS_FILE = "betweenness_centrality.tsv"
+
+CLUSTERTING_COLUMN = "Local clustering"
+DEGREE_COLUMN = "Node degree"
+CLOSENESS_COLUMN = "Closeness centrality"
 
 
 # Load graph into memory
@@ -36,16 +41,25 @@ def load_graphml_format(graph_path):
 def calculate_local_clustering(graph, save_file_name):
     print("Calculating words local clustering coefficient...")
     local_clustering = nx.clustering(graph)
-    cluster_values = list(map(lambda coeff: round(coeff, 4), local_clustering.values()))
+    save_features(local_clustering, CLUSTERTING_COLUMN, save_file_name)
 
-    word_coeff = np.concatenate((np.array(list(local_clustering.keys()),
+
+def calculate_node_degree(graph, save_file_name):
+    node_degree = graph.degree(weight="weight")
+    save_features(dict(node_degree), DEGREE_COLUMN, save_file_name)
+
+
+def save_features(feature_dict, feature_name, save_file_name):
+    feature_values = list(map(lambda coeff: round(coeff, 4), feature_dict.values()))
+
+    word_coeff = np.concatenate((np.array(list(feature_dict.keys()),
                                           ndmin=2).T,
-                                 np.array(cluster_values,
+                                 np.array(feature_values,
                                           ndmin=2).T),
                                 axis=1)
 
     df = pd.DataFrame(data=word_coeff,
-                      columns=["Word", "Local clustering"])
+                      columns=["Word", feature_name])
 
     df.to_csv(RESULTS_PATH + save_file_name,
               sep='\t',
@@ -53,6 +67,43 @@ def calculate_local_clustering(graph, save_file_name):
               index=False)
 
     print("Result is saved to {}".format(RESULTS_PATH + save_file_name))
+
+
+def read_features(clustering_file_name, node_file_name, closeness_file_name):
+    graph = nx.Graph(load(PAJEK_FORMAT)).to_undirected()
+    try:
+        print("Reading local clustering features...")
+        cluster_df = pd.read_csv(RESULTS_PATH + clustering_file_name,
+                                 sep='\t')
+    except FileNotFoundError:
+        print("Local clustering features file doesn't exist.")
+        calculate_local_clustering(graph, clustering_file_name)
+        cluster_df = pd.read_csv(RESULTS_PATH + clustering_file_name,
+                                 sep='\t')
+
+    try:
+        print("Reading node degree features...")
+        degree_df = pd.read_csv(RESULTS_PATH + node_file_name,
+                                sep='\t')
+    except FileNotFoundError:
+        print("Node degree features file doesn't exist.")
+        calculate_node_degree(graph, node_file_name)
+        degree_df = pd.read_csv(RESULTS_PATH + node_file_name,
+                                sep='\t')
+
+    try:
+        print("Reading closeness centrality features...")
+        closeness_df = pd.read_csv(RESULTS_PATH + closeness_file_name,
+                                   sep='\t')
+    except FileNotFoundError:
+        print("Closeness centrality feature file doesn't exist.")
+        save_features(closeness_centrality(graph),
+                      CLOSENESS_COLUMN,
+                      closeness_file_name)
+        closeness_df = pd.read_csv(RESULTS_PATH + closeness_file_name,
+                                   sep='\t')
+
+    return cluster_df, degree_df, closeness_df
 
 
 # Parse Pajek format graph file (.net) and get word day occurrences
@@ -78,3 +129,9 @@ def get_word_attributes(graph_file_path):
                     word_day_dict[vals[1]] = days
 
     return word_day_dict
+
+
+if __name__ == "__main__":
+    read_features("word_clustering_coefficient.tsv",
+                  "node_degree.tsv",
+                  "closeness_coefficient.tsv")
