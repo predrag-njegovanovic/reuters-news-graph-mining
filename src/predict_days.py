0e5f5f7db9e58e15ad62e45d05861e99ddbb40ba
Predrag Njegovanovic <djaps94@gmail.com>
2018-06-08 12:21:57 +0200

    Plot features distributions and correlations

--- src/predict_days.py
+++ src/predict_days.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+from scipy.stats.stats import spearmanr
 from src.utils import DATASET_PATH
 
 
@@ -10,5 +13,25 @@ def load_dataset(dataset_path):
     return data[:, 0:col_num-1], data[:, col_num-1]
 
 
-def analyze_dataset(dataset_path):
-    return None
+def plot_feature_distributions(features):
+    sns.distplot(features[:, 0])
+    plt.title("Local clustering distribution")
+    plt.show()
+
+    sns.distplot(features[:, 1])
+    plt.title("Node degree distribution")
+    plt.show()
+
+    sns.distplot(features[:, 2])
+    plt.title("Closeness centrality distribution")
+    plt.show()
+
+
+def features_correlation(features):
+    corr = spearmanr(features)[0]
+    sns.heatmap(corr,
+                annot=True,
+                linewidth=.5,
+                xticklabels=["Local clustering", "Node degree", "Closeness"],
+                yticklabels=["Local clustering", "Node degree", "Closeness"])
+    plt.show()
